89830499bcc5f80ace2df95ece8cf674f2287b10
Dan <sidato83@gmail.com>
2023-01-25 20:45:19 -0700

    Added jump with ground checking

--- Flashback/Assets/Scripts/PlayerController.cs
+++ Flashback/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Move Values")]
     [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Player Jump Values")]
+    [SerializeField] private float jumpVelocity = 3f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    [SerializeField] private Transform groundCheckCollider;
+    [SerializeField] private LayerMask groundLayer;
 
     private Transform tf;
     private Rigidbody2D rb2d;
@@ -24,12 +32,41 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         MovePlayer();
+        ImproveJumpPhysics();
     }
+
 
     private void CheckInput()
     {
         xInput = Input.GetAxis("Horizontal") * Time.deltaTime;
         yInput = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            playerJump();
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.CircleCast(groundCheckCollider.transform.position, 0.5f, Vector2.down, 0.1f, groundLayer);
+    }
+
+    private void playerJump()
+    {
+        rb2d.velocity = Vector2.up * jumpVelocity;
+    }
+
+    private void ImproveJumpPhysics()
+    {
+        if (rb2d.velocity.y < 0)
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
 
     private void MovePlayer()
